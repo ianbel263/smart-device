2f86b76ae3216d94dcc9030e54a1e132872b11af
Andrey Belyaev <ianbel263@gmail.com>
2020-01-27 09:39:24 +0300

    Добавляет полифилл для ie11

---
+++ js/polyfill.object-fit.js
@@ -0,0 +1,10 @@
+//ES5 version transpiled from code above with BabelJS
+if ('objectFit' in document.documentElement.style === false) {
+    document.addEventListener('DOMContentLoaded', function() {
+      Array.prototype.forEach.call(document.querySelectorAll('.logo img'), function(image) {
+        (image.runtimeStyle || image.style).background = "url(\"".concat(image.src, "\") no-repeat 50%/").concat(image.currentStyle ? image.currentStyle['object-fit'] : image.getAttribute('data-object-fit'))
+        image.src = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='".concat(image.width, "' height='").concat(image.height, "'%3E%3C/svg%3E")
+      })
+    })
+  }
+  
