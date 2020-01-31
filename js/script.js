2f697536703dba996d27e3ccca4e2c50f6d680e9
Andrey Belyaev <ianbel263@gmai.com>
2020-01-31 09:01:31 +0300

    111

--- js/script.js
+++ js/script.js
@@ -164,6 +164,33 @@ if (anchorLinks) {
   });
 }
 
+var createPhoneNumberMask = function (evt, phoneInput) {
+  if (!(evt.key === 'ArrowLeft' || evt.key === 'ArrowRight' || evt.key === 'Backspace' || evt.key === 'Tab')) {
+    evt.preventDefault();
+  }
+
+  var mask = '+7(111)111-11-11';
+
+  if (/[0-9\+\ \-\(\)]/.test(evt.key)) {
+    var currentString = phoneInput.value;
+    var currentLength = currentString.length;
+
+    if (/[0-9]/.test(evt.key)) {
+      if (mask[currentLength] === '1') {
+        phoneInput.value = currentString + evt.key;
+      } else {
+        for (var i = currentLength; i < mask.length; i++) {
+          if (mask[i] === '1') {
+            phoneInput.value = currentString + evt.key;
+            break;
+          }
+          currentString += mask[i];
+        }
+      }
+    }
+  }
+};
+
 var phoneInputs = document.querySelectorAll('input[name=phone-number]');
 
 if (phoneInputs) {
@@ -181,30 +208,7 @@ if (phoneInputs) {
     });
 
     phoneInput.addEventListener('keydown', function (evt) {
-      if (!(evt.key === 'ArrowLeft' || evt.key === 'ArrowRight' || evt.key === 'Backspace' || evt.key === 'Tab')) {
-        evt.preventDefault();
-      }
-
-      var mask = '+7(111)111-11-11';
-
-      if (/[0-9\+\ \-\(\)]/.test(evt.key)) {
-        var currentString = phoneInput.value;
-        var currentLength = currentString.length;
-
-        if (/[0-9]/.test(evt.key)) {
-          if (mask[currentLength] === '1') {
-            phoneInput.value = currentString + evt.key;
-          } else {
-            for (var i = currentLength; i < mask.length; i++) {
-              if (mask[i] === '1') {
-                phoneInput.value = currentString + evt.key;
-                break;
-              }
-              currentString += mask[i];
-            }
-          }
-        }
-      }
+      createPhoneNumberMask(evt, phoneInput);
     });
   });
 }
@@ -212,19 +216,25 @@ if (phoneInputs) {
 var companyElement = document.querySelector('.page-footer__company');
 var companyTextElement = companyElement.querySelector('span');
 var copyrightElement = document.querySelector('.page-footer__copyright');
-var newCompanyTextElement = companyTextElement.cloneNode(true);
+// var newCompanyTextElement = companyTextElement.cloneNode(true);
 
-companyElement.classList.remove('page-footer__company--nojs');
-copyrightElement.classList.remove('page-footer__copyright--nojs');
-
-var screenWidth = document.body.clientWidth;
-
-if (screenWidth > TABLET_MAX_WIDTH) {
-  copyrightElement.appendChild(newCompanyTextElement);
+if (companyElement) {
+  companyElement.classList.remove('page-footer__company--nojs');
 }
 
+if (copyrightElement) {
+  copyrightElement.classList.remove('page-footer__copyright--nojs');
+}
+
+var replaceElement = function (parent, element) {
+  var newElement = element.cloneNode(true);
+  parent.appendChild(newElement);
+};
+
+// replaceElement(copyrightElement, companyTextElement);
+
 window.addEventListener('resize', function () {
-  if (screenWidth > TABLET_MAX_WIDTH) {
-    copyrightElement.appendChild(newCompanyTextElement);
+  if (document.body.clientWidth > TABLET_MAX_WIDTH) {
+    replaceElement(copyrightElement, companyTextElement);
   }
 });
